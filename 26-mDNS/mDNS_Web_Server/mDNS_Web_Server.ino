74aae3be3e73400ada5f90dee44497de76fd5d72
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2026-03-17 11:28:47 +0300

    Выведена таблица адресов

--- 26-mDNS/mDNS_Web_Server/mDNS_Web_Server.ino
+++ 26-mDNS/mDNS_Web_Server/mDNS_Web_Server.ino
@@ -39,7 +39,8 @@ const char* password = "18009217";
 // TCP server at port 80 will respond to HTTP requests
 NetworkServer server(80);
 
-void setup(void) {
+void setup(void) 
+{
   Serial.begin(115200);
 
   // Connect to WiFi network
@@ -47,7 +48,8 @@ void setup(void) {
   Serial.println("");
 
   // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED) 
+  {
     delay(500);
     Serial.print(".");
   }
@@ -62,7 +64,8 @@ void setup(void) {
   //   the fully-qualified domain name is "esp32.local"
   // - second argument is the IP address to advertise
   //   we send our IP address on the WiFi network
-  if (!MDNS.begin("esp32")) {
+  if (!MDNS.begin("esp32")) 
+  {
     Serial.println("Error setting up MDNS responder!");
     while (1) {
       delay(1000);
@@ -78,17 +81,20 @@ void setup(void) {
   MDNS.addService("http", "tcp", 80);
 }
 
-void loop(void) {
+void loop(void) 
+{
   // Check if a client has connected
   NetworkClient client = server.accept();
-  if (!client) {
+  if (!client) 
+  {
     return;
   }
   Serial.println("");
   Serial.println("New client");
 
   // Wait for data from client to become available
-  while (client.connected() && !client.available()) {
+  while (client.connected() && !client.available()) 
+  {
     delay(1);
   }
 
@@ -99,7 +105,8 @@ void loop(void) {
   // Retrieve the "/path" part by finding the spaces
   int addr_start = req.indexOf(' ');
   int addr_end = req.indexOf(' ', addr_start + 1);
-  if (addr_start == -1 || addr_end == -1) {
+  if (addr_start == -1 || addr_end == -1) 
+  {
     Serial.print("Invalid request: ");
     Serial.println(req);
     return;
@@ -109,14 +116,17 @@ void loop(void) {
   Serial.println(req);
 
   String s;
-  if (req == "/") {
+  if (req == "/") 
+  {
     IPAddress ip = WiFi.localIP();
     String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
     s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>Hello from ESP32 at ";
     s += ipStr;
     s += "</html>\r\n\r\n";
     Serial.println("Sending 200");
-  } else {
+  } 
+  else 
+  {
     s = "HTTP/1.1 404 Not Found\r\n\r\n";
     Serial.println("Sending 404");
   }
